b1fd3b039c4e8101c218c1b6dd347ce6acc03ad5
Jefferson Abreu Martinez <abreumartinezjefferson@gmail.com>
2024-05-19 14:32:40 -0400

    Feature/Email Done

---
+++ Auth.Infraestructure.Identity/Features/Email/Commands/SendEmail/SendEmailCommand.cs
@@ -0,0 +1,52 @@
+﻿using Auth.Core.Application.Settings;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MediatR;
+using MimeKit;
+
+namespace Auth.Infraestructure.Identity.Features.Email.Commands.SendEmail
+{
+    public class SendEmailCommand : IRequest<bool>
+    {
+        public required string To { get; set; }
+        public required string Subject { get; set; }
+        public required string Body { get; set; }
+    }
+    public class SendEmailCommandHandler : IRequestHandler<SendEmailCommand, bool>
+    {
+        private MailSettings _mailSettings { get; }
+        public SendEmailCommandHandler(MailSettings mailSettings)
+        {
+            _mailSettings = mailSettings;
+        }
+
+        public async Task<bool> Handle(SendEmailCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                MimeMessage email = new();
+                email.Sender = MailboxAddress.Parse($"{_mailSettings.DisplayName} <{_mailSettings.EmailFrom}>");
+                email.To.Add(MailboxAddress.Parse(request.To));
+                email.Subject = request.Subject;
+                BodyBuilder bodyBuilder = new();
+                bodyBuilder.HtmlBody = request.Body;
+                email.Body = bodyBuilder.ToMessageBody();
+
+
+                using SmtpClient smtp = new();
+                smtp.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                smtp.Connect(_mailSettings.SmtpHost, _mailSettings.SmtpPort, SecureSocketOptions.StartTls, cancellationToken);
+                smtp.Authenticate(_mailSettings.SmtpUser, _mailSettings.SmtpPassword, cancellationToken);
+                await smtp.SendAsync(email);
+                smtp.Disconnect(true, cancellationToken);
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ Auth.Infraestructure.Identity/Features/Register/Commands/CreateAccount/CreateAccountCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace Auth.Infraestructure.Identity.Features.Register.Commands.CreateAccount
+{
+    public class CreateAccountCommand : IRequest<string>
+    {
+        public required string Name { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public string ImageProfile { get; set; } = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+        public required string ConfirmPassword { get; set; }
+        public required string Phone { get; set; }
+    }
+    public class CreateAccountCommandHandler : IRequestHandler<CreateAccountCommand, string>
+    {
+        
+    }
+
+}
