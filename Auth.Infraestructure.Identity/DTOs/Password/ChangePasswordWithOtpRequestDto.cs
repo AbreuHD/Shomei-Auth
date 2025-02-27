a3bc33553020e22212ef2edec6dd7aa25fe42ded
Jefferson Abreu <abreumartinezjefferson@gmail.com>
2025-02-27 17:30:00 -0400

    Update pipeline version and enhance password reset flow
    - Bump pipeline version in `TestJenkins` to "0.1.5".
    - Remove unused folder reference and image item in `Auth.Infraestructure.Identity.csproj`.
    - Introduce `PasswordResetEmail` class for email content generation.
    - Add `GeneratePasswordResetOtp` endpoint in `AccountController.cs`.
    - Create DTOs: `ChangePasswordWithOtpRequestDto` and `PasswordChangeOtpRequestDto`.
    - Implement `ChangePasswordWithOtpCommand` and `GeneratePasswordResetOtpCommand` for password management and OTP generation.

---
+++ Auth.Infraestructure.Identity/DTOs/Password/ChangePasswordWithOtpRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Infraestructure.Identity.DTOs.Password
+{
+    public class ChangePasswordWithOtpRequestDto
+    {
+        public required string NewPassword { get; set; }
+        public required string RepeatNewPassword { get; set; }
+        public required string Otp { get; set; }
+        public required string Email { get; set; }
+    }
+}
